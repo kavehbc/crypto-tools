b9c5ad0f58683d50076f89c40a2f86fcdc5824b8
Kaveh Bakhtiyari <kavehbc@yahoo.com>
2022-12-16 23:50:05 -0500

    Download RSA keys

--- apps/generate_keys.py
+++ apps/generate_keys.py
@@ -13,47 +13,50 @@ def utf8(s: bytes):
 
 def main():
     st.title("Generate RSA Keys")
-    key_size_options = [128, 256, 1024, 2048, 4096]
+    key_size_options = [128, 256, 1024, 2048, 4096, 8192]
+    key_size = int(st.selectbox("Key Size (bit)", options=key_size_options, index=len(key_size_options) - 1))
+    st.caption(f"RSA can encrypt up to key size bits minus OAEP Padding (42) bytes")
+    st.caption(f"{key_size}-bit RSA can encrypt up to {(key_size / 8) - 42:,.0f} bytes")
+    btn_generate = st.button("Generate Keys")
 
-    key_size = int(st.selectbox("Key Size", options=key_size_options, index=len(key_size_options) - 1))
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=key_size,
-        backend=default_backend()
-    )
-    public_key = private_key.public_key()
+    if btn_generate:
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=key_size,
+            backend=default_backend()
+        )
+        public_key = private_key.public_key()
 
-    private_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
+        private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        st.session_state['private_pem'] = private_pem
 
-    # with open('private_key.pem', 'wb') as f:
-    #     f.write(private_pem)
+        public_pem = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        st.session_state['public_pem'] = public_pem
 
-    public_pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
+    if 'public_pem' in st.session_state:
+        private_pem = st.session_state['private_pem']
+        public_pem = st.session_state['public_pem']
 
-    # with open('public_key.pem', 'wb') as f:
-    #     f.write(public_pem)
+        with st.expander("Private Key"):
+            st.code(private_pem.decode("utf-8"))
+        with st.expander("Public Key"):
+            st.code(public_pem.decode("utf-8"))
 
-    with st.expander("Private Key"):
-        st.code(private_pem.decode("utf-8"))
-    with st.expander("Public Key"):
-        st.code(public_pem.decode("utf-8"))
-
-    st.button("Refresh Keys")
-    # st.download_button("Download Private Key",
-    #                    data=private_pem,
-    #                    file_name="private_key.pem",
-    #                    mime="application/x-pem-file")
-    # st.download_button("Download Public Key",
-    #                    data=public_pem,
-    #                    file_name="public_key.pem",
-    #                    mime="application/x-pem-file")
+        st.download_button("Download Private Key",
+                           data=private_pem,
+                           file_name="private_key.pem",
+                           mime="application/x-pem-file")
+        st.download_button("Download Public Key",
+                           data=public_pem,
+                           file_name="public_key.pem",
+                           mime="application/x-pem-file")
 
 
 if __name__ == '__main__':
